bb9873818d2d5a1102358b9fcca3449d73248c3c
Chely Ho <adept_inept@yahoo.com>
2017-11-15 22:14:39 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -30,8 +30,8 @@ class Question {
   static async Find(id){
     console.log(`Waiting for Question ${id} to load...`)
 
-        setTimeout(function(){
-        const question = await Question._All[id-1] // Do Not Edit
+        await setTimeout(function(){
+        const question = Question._All[id-1] // Do Not Edit
         console.log(`...Question ${id} Loaded`) // Do Not Edit
 
         return question
