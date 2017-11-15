418bd9ffaf195eccacd246b171bd72c135553ba7
Chely Ho <adept_inept@yahoo.com>
2017-11-15 22:02:22 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -27,14 +27,14 @@ class Question {
   }
 
   // FIXME: Implement Promise
-  static async Find(await id){
+  static async Find(id){
     console.log(`Waiting for Question ${id} to load...`)
 
       setTimeout(function(){
         const question = Question._All[id-1] // Do Not Edit
         console.log(`...Question ${id} Loaded`) // Do Not Edit
 
-        return question 
+        return question
       }, 1500)
 
   }
